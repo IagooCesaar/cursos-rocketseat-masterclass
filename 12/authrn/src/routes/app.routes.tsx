83055c2d2b28be8923345786926b595d5d4f9320
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-04-24 23:38:35 -0300

    Criação de componente base

--- 12/authrn/src/routes/app.routes.tsx
+++ 12/authrn/src/routes/app.routes.tsx
@@ -1,2 +1,14 @@
 // Rotas que usuário poderá navegar após ele não estiver autenticado
 import Dashboard from '../pages/Dashboard';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <AppStack.Navigator>
+    <AppStack.Screen name="Dashboard" component={Dashboard} />
+  </AppStack.Navigator>
+);
+
+export default AppRoutes;
