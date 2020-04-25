5f9aa85b8793221fed9a1d95482db66b5d3cb785
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-04-24 23:33:43 -0300

    Preparativos para inciar rotas
    Início de criação de rotas para pré e pós autenticação do usuário

---
+++ 12/authrn/src/routes/app.routes.tsx
@@ -0,0 +1,2 @@
+// Rotas que usuário poderá navegar após ele não estiver autenticado
+import Dashboard from '../pages/Dashboard';
---
+++ 12/authrn/src/routes/auth.routes.tsx
@@ -0,0 +1,2 @@
+// Rotas que usuário poderá navegar enquanto ele não estiver autenticado
+import SignIn from '../pages/SignIn';
