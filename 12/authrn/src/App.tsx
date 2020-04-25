74d00df93fa40beb7f96a7c6ce8c5a90761f1040
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-04-24 23:18:15 -0300

    Update App.tsx
    Orientações da documentação

--- 12/authrn/src/App.tsx
+++ 12/authrn/src/App.tsx
@@ -2,9 +2,14 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
 };
 
 export default App;
