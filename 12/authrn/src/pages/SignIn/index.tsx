cb55b6cab92d5b5b4e21acd7bd06f58addcfd170
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-04-24 23:54:05 -0300

    Criada função para simular autenticação em API

--- 12/authrn/src/pages/SignIn/index.tsx
+++ 12/authrn/src/pages/SignIn/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 
-const SignIn: React.FC = () => (
-  <View>
-    <Button title="Sign In" onPress={() => {}} />
-  </View>
-);
+import * as authService from '../../services/auth';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const SignIn: React.FC = () => {
+  async function handleSigIn() {
+    // Ex: email e password para autenticação
+    const response = await authService.signIn();
+    console.log(response);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign In" onPress={handleSigIn} />
+    </View>
+  );
+};
 
 export default SignIn;
